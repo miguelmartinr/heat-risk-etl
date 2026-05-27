eeb3dc842ad8b65253af6ca7b0ea71e64cedca2e
Miguel Martin <miguelmartinr@users.noreply.github.com>
2026-05-27 11:55:19 +0200

    NEW Changes
    The code from extract.py has been modified so as that
    the data extracted from the API gets saved.
    
    On the other hand, the code transform.py has been added, which contains the function defined to transform
    the data from the API.

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def extract_data():
     url = "https://api.openweathermap.org/data/2.5/weather"
@@ -7,11 +8,21 @@ def extract_data():
         "appid": 'cfd268431d027c3ce8a8520557211d60',
         'units': 'metric'
     }
+
     response = requests.get(url, params)
+    
     if response.status_code == 200:
-        return response.json()
+        data= response.json()
+
+        with open('moralzarzal_raw.json', mode='w', encoding='utf-8') as save_file:
+            json.dump(data, save_file, indent=4)
+
+        return data
+
     elif response.status_code == 401:
+
         print('Error: Authentication Problem')
+
     else:
         print('Unexpected error')
-
+    
---
+++ src/transform.py
@@ -0,0 +1,12 @@
+# Transform
+
+def transform_data(data):
+    temp_madrid = {
+        'temperatura': data['main']['temp'],
+        'humedad': data['main']['humidity'],
+        'viento': data['wind']['speed'],
+        'sensacion_termica': data['main']['feels_like'],
+        'IRC': (data['main']['feels_like'])+((data['main']['humidity'])/10)
+    }
+
+    return temp_madrid
