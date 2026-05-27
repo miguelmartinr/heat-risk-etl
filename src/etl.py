5ea29fcc755497ed904fc6f41490bd0863d80ae2
Miguel Martin <miguelmartinr@users.noreply.github.com>
2026-05-27 12:23:26 +0200

    Created new .py file.
    The last file where the ETL runs.
    
    As a small addition, the .py file with the database
    connection has been renamed as dbconn.

---
+++ src/etl.py
@@ -0,0 +1,20 @@
+from extract import extract_data
+from transform import transform_data
+from dbconn import db_connexion
+from load import load_data
+
+def init_etl():
+    print('Iniciando ETL...')
+
+    data = extract_data()
+    print('Datos extraidos correctamente')
+
+    data_trans = transform_data(data)
+    print('Datos transformados con exito')
+
+    conn = db_connexion
+    load_data(conn, transform_data)
+    conn.close()
+
+if __name__ == '__main__':
+    init_etl()    
